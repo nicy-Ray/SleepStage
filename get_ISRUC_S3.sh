711a9e710f18c533eb1ce131b21b41c823193594
nicy-Ray <114130224+nicy-Ray@users.noreply.github.com>
2025-03-12 22:03:53 +0800

    Update get_ISRUC_S3.sh

--- get_ISRUC_S3.sh
+++ get_ISRUC_S3.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 mkdir -p ./data/ISRUC_S3/ExtractedChannels
 mkdir -p ./data/ISRUC_S3/RawData
 echo 'Make data dir: ./data/ISRUC_S3'
 
 cd ./data/ISRUC_S3/RawData
-for s in {1..10}
+for s in $(seq 1 10)  # 修正 for 语法
 do
     wget http://dataset.isr.uc.pt/ISRUC_Sleep/subgroupIII/$s.rar
     unrar x $s.rar
 done
 echo 'Download Data to "./data/ISRUC_S3/RawData" complete.'
 
-cd ./data/ISRUC_S3/ExtractedChannels
-for s in {1..10}
+cd ../ExtractedChannels  # 修改 cd 目录，避免错误
+for s in $(seq 1 10)  # 修正 for 语法
 do
     wget http://dataset.isr.uc.pt/ISRUC_Sleep/ExtractedChannels/subgroupIII-Extractedchannels/subject$s.mat
 done
